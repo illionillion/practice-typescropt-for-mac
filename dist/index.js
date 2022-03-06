c3736cbdb43e37d9db1eedc440217b5f00301078
yuto ichinose <yuto.0526.sano@gmail.com>
2022-03-06 22:49:10 +0900

    九九表を書いた

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,18 @@ function hello(name) {
     return `Hello,${name}`;
 }
 console.log(hello('World'));
+function addzero(text) {
+    if (text.length < 3) {
+        return `0${text}`;
+    }
+    return text;
+}
+//九九表
+for (let i = 1; i <= 9; i++) {
+    let text = '';
+    for (let j = 1; j <= 9; j++) {
+        text += addzero(`${i * j} `);
+    }
+    console.log(text);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,22 @@ function hello(name:string):string{
     return `Hello,${name}`
 }
 
-console.log(hello('World'));
+console.log(hello('World'))
+
+function addzero(text:string):string{
+    if (text.length<3) {
+        return `0${text}`
+    }
+    return text
+}
+
+//九九表
+for (let i = 1; i <= 9; i++) {
+
+    let text:string=''
+    for (let j = 1; j <= 9; j++) {
+        text+=addzero(`${i * j} `)
+    }
+    console.log(text)
+    
+}
